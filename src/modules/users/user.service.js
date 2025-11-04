392e66ceb5057321029cfd067ff4b8aebb0691da
IgorSasaki <igor-sasaki@hotmail.com>
2025-11-03 21:05:45 -0300

    Implement user services

--- src/modules/users/user.service.js
+++ src/modules/users/user.service.js
@@ -9,22 +9,30 @@ export const makeUserService = () => {
 
     const register = async ({ name, email, password }) => {
         const exists = await repo.findByEmail(email);
+
         if (exists) throw new HttpError('Email already in use', 409, 'EMAIL_TAKEN');
+
         const passwordHash = await bcrypt.hash(password, 10);
+
         return repo.create({ name, email, passwordHash });
     };
 
     const login = async ({ email, password }) => {
         const user = await repo.findByEmail(email);
+
         if (!user) throw new HttpError('Invalid credentials', 401, 'INVALID_CREDENTIALS');
+
         const ok = await bcrypt.compare(password, user.passwordHash);
+
         if (!ok) throw new HttpError('Invalid credentials', 401, 'INVALID_CREDENTIALS');
+
         const accessToken = jwt.sign({}, env.jwtSecret, {
             subject: String(user.id),
             expiresIn: env.jwtExpiresIn
         });
+        
         return { accessToken };
     };
-    
+
     return { register, login };
 };
